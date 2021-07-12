45a269dde3352d1b9dfcefa7f586a6c4b743aa47
FadiAshy <ashyfadi@gmail.com>
2021-07-12 12:03:10 +0300

    Full Project v1.0

---
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    Animator animator;
+
+    public bool run;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void MovePlayer()
+    {
+        if (!run)
+        {
+            animator?.SetBool("Run", true);
+            run = true;
+        }
+    }
+
+    public void StopPlayer()
+    {
+        if (run)
+        {
+            animator?.SetBool("Run", false);
+            run = false;
+        }
+    }
+    public void PlayerDead()
+    {
+        animator?.SetTrigger("Dead");
+    }
+}
---
+++ Assets/Scripts/Player/PlayerSpriteSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This Class to spawn the correct Player Sprite/Object Prefab
+
+public class PlayerSpriteSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject Elf_M_Prefab;
+    [SerializeField] GameObject Elf_F_Prefab;
+    [SerializeField] GameObject Lizard_Prefab;
+
+    GameObject currentPlayer;
+
+    public void SpawnPlayer()
+    {
+        if (FindObjectOfType<PlayerState>().isCrewMate)
+            StartGameCrewmate();
+        else
+            StartGameImposter();
+    }
+
+    public void StartGameCrewmate()
+    {
+        if (Random.Range(0, 100) > 50)
+            currentPlayer = Instantiate(Elf_M_Prefab, this.transform);
+        else
+            currentPlayer = Instantiate(Elf_F_Prefab, this.transform);
+    }
+
+    void StartGameImposter()
+    {
+        currentPlayer = Instantiate(Lizard_Prefab, this.transform);
+    }
+}
